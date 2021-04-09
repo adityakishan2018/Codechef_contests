64ecff4d11cce5ab8c3f259aea3f8710ec5eaa86
aditya kishan <kishan.aditya2017@gmail.com>
2021-04-09 12:29:21 +0530

    Create sol3.cpp

---
+++ sol3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+
+	long int R=0,O=0,C=0;
+		
+		cin>>R>>O>>C;
+		
+		if((20-O)*6*6>(R-C))
+		{
+			
+			cout<<"yes"<<endl;
+		}
+		else{
+			cout<<"No"<<endl;
+		}
+		
+	
+	
+	
+	return 0;
+}
